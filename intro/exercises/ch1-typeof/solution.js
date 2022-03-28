b4c7ec1e18ed850c4f4c5d26c3798985c5ee70e1
Dominic <dominicvana@gmail.com>
2022-03-28 13:20:33 +0100

    Feat: typeof intro challenge

---
+++ intro/exercises/ch1-typeof/solution.js
@@ -0,0 +1,9 @@
+console.log(typeof 7.0 === 'number');
+console.log(typeof 22 === 'number');
+console.log(typeof true === 'boolean');
+
+var myVar;
+console.log(typeof myVar === 'undefined');
+
+myVar = 'Dom'; //add your name here
+console.log(typeof myVar === 'string');
--- intro/exercises/ch1-typeof/typeof.js
+++ intro/exercises/ch1-typeof/typeof.js
@@ -1,18 +1,23 @@
 /* ========== *
  * Challenge 1 *
  * ===========
+ *
  * Replace the blank underscores inside the console logs with the data type
- * (i.e. 'string') you think each value will be. (Leave the quotes) */
+ * (i.e. 'string') you think each value will be. (Leave the quotes)
+ *
+ * To test your solutions run
+ * node typeof.js
+ * from the command line (make sure you're in this directory when you do)
+ *
+ * You should only see trues being logged
+ * */
 
-//Uncomment the lines below to test your code
-// console.log(typeof 7.0 === "_______")
-// console.log(typeof 22 === "_______")
-// console.log(typeof true === "_______")
+console.log(typeof 7.0 === '_______');
+console.log(typeof 22 === '_______');
+console.log(typeof true === '_______');
 
 var myVar;
-// console.log(typeof myVar === "_______")
+console.log(typeof myVar === '_______');
 
-//myVar = //add your name here
-// console.log(typeof myVar === "_______")
-
-//Comment them back out with two forward slashes at the beginning once you have finished
+myVar = //add your name here
+  console.log(typeof myVar === '_______');
