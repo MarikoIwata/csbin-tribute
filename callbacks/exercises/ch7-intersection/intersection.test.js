765b5543b35518b25e8ce32b7750ae4a83e458b3
Dominic <dominicvana@gmail.com>
2022-03-29 16:47:41 +0100

    Refactor: intersection() test

--- callbacks/exercises/ch7-intersection/intersection.test.js
+++ callbacks/exercises/ch7-intersection/intersection.test.js
@@ -2,23 +2,24 @@ let intersection;
 intersection = require('./intersection');
 // intersection = require('./solution'); // uncomment to test solution file
 
-const mocks = {
-  arg: [
+describe('intersection(arrays)', () => {
+  // Arrange
+  const data = [
     [5, 10, 15, 20],
     [15, 88, 1, 5, 7],
     [1, 10, 15, 5, 20],
-  ],
-  expectedOutput: [5, 15],
-};
+  ];
+  const expected = [5, 15];
 
-describe('intersection(arrays) challenge', () => {
-  const result = intersection(mocks.arg);
+  // Act
+  const result = intersection(data);
 
+  // Assert
   test('should return an array', () => {
-    expect(Array.isArray(result)).toBe(true);
+    expect(result).toBeInstanceOf(Array);
   });
 
-  test('array should contain elements appearing in all of the passed-in arrays', () => {
-    expect(result).toEqual(mocks.expectedOutput);
+  test('the array should contain elements appearing in all of the passed-in arrays', () => {
+    expect(result).toEqual(expected);
   });
 });
