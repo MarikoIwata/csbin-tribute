30abcab18f5854c7f354133e19781eb322192e80
Dominic <dominicvana@gmail.com>
2022-03-29 18:08:37 +0100

    Chore: add to forEach() instrutions

--- callbacks/exercises/ch4-for-each/for_each.js
+++ callbacks/exercises/ch4-for-each/for_each.js
@@ -4,6 +4,10 @@ Challenge 4
 Create a function called forEach that takes an array and a callback, 
 and runs the callback on each element of the array. forEach does not return anything.
 
+The tests require you to handle all of the callback arguments that the 
+real .forEach() method allows. Link to docs:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
 */
 
 // To test your solution run
