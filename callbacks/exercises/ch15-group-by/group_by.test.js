af571a44525477bd3c7425ebbf99704e9e5ff39c
Dominic <dominicvana@gmail.com>
2022-03-29 23:01:23 +0100

    Refactor: groupBy() tests

--- callbacks/exercises/ch15-group-by/group_by.test.js
+++ callbacks/exercises/ch15-group-by/group_by.test.js
@@ -3,48 +3,52 @@ groupBy = require('./group_by');
 // groupBy = require('./solution'); // uncomment to test solution file
 const isObject = require('../../../utils/values/is_object');
 const arrayToNumbers = require('../../../utils/values/array_to_numbers');
+const unique = require('../../../utils/values/uniques');
 
-const mocks = {
-  decimals: [1.3, 2.1, 2.4],
-  callback: jest.fn(function floored(num) {
-    return Math.floor(num);
-  }),
-  expectedResult: { 1: [1.3], 2: [2.1, 2.4] },
-};
+function setup() {
+  return {
+    data: [1.3, 2.1, 2.4],
+    mockCb: jest.fn(function floored(num) {
+      return Math.floor(num);
+    }),
+    expected: { 1: [1.3], 2: [2.1, 2.4] },
+  };
+}
 
 describe('groupBy(array, callback)', () => {
-  const { decimals, callback } = mocks;
-  const result = groupBy(decimals, callback);
+  const { data, mockCb } = setup();
+
+  const result = groupBy(data, mockCb);
 
   test('should return an object', () => {
     expect(isObject(result)).toBe(true);
   });
-  test('should perform the callback on each element of the array', () => {
-    const cbCalls = callback.mock.calls.length;
-    const expectedCbCalls = decimals.length;
 
-    expect(cbCalls).toBe(expectedCbCalls);
+  test('should perform the callback on each element of the array', () => {
+    expect(mockCb).toHaveBeenCalledTimes(data.length);
   });
 
   test('each returned value from the callback should be a key on the object', () => {
-    const cbResults = [
-      ...new Set(callback.mock.results.map(({ value }) => value)),
-    ];
+    const cbResults = unique(
+      mockCb.mock.results.map(({ value }) => value)
+    );
     // keys need to be coerced from strings to numbers for comparison
     const keys = arrayToNumbers(Object.keys(result));
+
     expect(cbResults).toEqual(keys);
   });
 
-  test('values of the returned object should be arrays', () => {
+  test("returned object's values should be arrays", () => {
     const values = Object.values(result);
 
     values.forEach((value) => {
-      expect(Array.isArray(value)).toBe(true);
+      expect(value).toBeInstanceOf(Array);
     });
   });
 
-  test('the arrays should contain the elements that resulted in their key being returned from the callback.', () => {
-    const { expectedResult } = mocks;
-    expect(result).toEqual(expectedResult);
+  test('arrays should contain elements that resulted in their key being returned from the callback.', () => {
+    const { expected } = setup();
+
+    expect(result).toEqual(expected);
   });
 });
