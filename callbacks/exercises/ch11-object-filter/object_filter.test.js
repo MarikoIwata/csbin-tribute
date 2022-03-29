f4f50eb7b11ebe02cd5a8dbd11f160875958f371
Dominic <dominicvana@gmail.com>
2022-03-29 22:19:53 +0100

    Refactor: majority() tests

--- callbacks/exercises/ch11-object-filter/object_filter.test.js
+++ callbacks/exercises/ch11-object-filter/object_filter.test.js
@@ -1,23 +1,24 @@
 let objectFilter;
-objectFilter = require('./object_filter');
-// objectFilter = require('./solution'); // uncomment to test solution file
+// objectFilter = require('./object_filter');
+objectFilter = require('./solution'); // uncomment to test solution file
 const isObject = require('../../../utils/values/is_object');
 
 const mocks = {
   object: { London: 'LONDON', LA: 'Los Angeles', Paris: 'PARIS' },
   callback: jest.fn((v) => v.toUpperCase()),
-  expectedOutput: { London: 'LONDON', Paris: 'PARIS' },
+  expected: { London: 'LONDON', Paris: 'PARIS' },
 };
 
-describe('objectFilter()', () => {
-  const { object, callback, expectedOutput } = mocks;
-  const output = objectFilter(object, callback);
+describe('objectFilter(obj, callback)', () => {
+  const { object, callback, expected } = mocks;
+
+  const result = objectFilter(object, callback);
 
   test('should return an object', () => {
-    expect(isObject(output)).toBe(true);
+    expect(isObject(result)).toBe(true);
   });
 
   test('object should contain props from input object if its value is equal to its key passed into the callback', () => {
-    expect(output).toEqual(expectedOutput);
+    expect(result).toEqual(expected);
   });
 });
--- callbacks/exercises/ch12-majority/majority.test.js
+++ callbacks/exercises/ch12-majority/majority.test.js
@@ -2,35 +2,44 @@ let majority;
 majority = require('./majority');
 // majority = require('./solution'); // uncomment to test solution file
 
-const mocks = {
-  arr1: [1, 2, 3, 5, 4],
-  arr2: [1, 2, 12, 5, 8],
-  arr3: [2, 3, 6, 5],
-  callback: jest.fn(function isOdd(x) {
-    return x % 2 === 1;
-  }),
-};
+function setup() {
+  return {
+    data: [
+      [1, 2, 3, 5, 4],
+      [1, 2, 12, 5, 8],
+      [2, 3, 6, 5],
+    ],
+    mockCb: jest.fn(function isOdd(x) {
+      return x % 2 === 1;
+    }),
+  };
+}
 
-describe('majority(array, callback)', () => {
-  const { arr1, arr2, arr3, callback } = mocks;
-  const expectedTrue = majority(arr1, callback);
-  const expectedFalse = majority(arr2, callback);
-  const equalTruesFalses = majority(arr3, callback);
+describe('majority(array, callback) should:', () => {
+  const { data, mockCb } = setup();
 
-  test('should return a boolean', () => {
-    expect(typeof expectedTrue).toBe('boolean');
-    expect(typeof expectedFalse).toBe('boolean');
+  const results = data.map((array) => majority(array, mockCb));
+
+  test('return a boolean', () => {
+    results.forEach((result) => {
+      expect(typeof result).toBe('boolean');
+    });
   });
 
-  test('should return true if majority of elements return true when passed to callback', () => {
-    expect(expectedTrue).toBe(true);
-    expect(expectedFalse).toBe(false);
-  });
-  test('if number of true and false returns are equal, then majority(...) should return false', () => {
-    expect(equalTruesFalses).toBe(false);
+  test('return true if majority of elements return true when passed to callback', () => {
+    expect(results[0]).toBe(true);
   });
 
-  test('majority(...) should short-circuit once a majority of true/false values has been established', () => {
-    expect(callback.mock.calls).not.toContainEqual([arr1.at(-1)]);
+  test('return false if majority of elements return false when passed to callback', () => {
+    expect(results[1]).toBe(false);
+  });
+
+  test('return false if number of trues and falses are equal', () => {
+    expect(results.at(-1)).toBe(false);
+  });
+
+  test('short-circuit once a majority of true/false values has been established', () => {
+    // majority of odds confirmed by second-to-last element in first data array
+    expect(mockCb).not.toHaveBeenCalledWith(data[0].at(-1));
   });
 });
